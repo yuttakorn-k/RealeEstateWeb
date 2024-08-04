f8d785331f153fabbe42cfac727878bf0755db50
yuttakorn-k <yuttakorn.kaey@e-tech.ac.th>
2024-08-04 13:43:15 +0700

    Delete backend directory

--- backend/routes/residencyRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createResidency, getAllResidencies, getResidency } from '../controllers/residenCtrl.js';
-const router = express.Router()
-
-router.post("/create", createResidency)
-router.get("/allres", getAllResidencies)
-router.get("/:id", getResidency)
-
-export {router as residencyRoute}
